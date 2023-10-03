16083e4a410257037968a8f5399522b64532a7f4
jaeseokk <kakaokokoa9971@gmail.com>
2023-10-03 14:18:38 +0900

    create test file

--- testQ1.py
+++ testQ1.py
@@ -1,8 +1,16 @@
-name = input()
+cars = [1, 11, 12, 6, 2, 3, 13, 7, 14, 4, 8, 5, 15, 9, 10]
 
-for s in name:
-    print(ord(s))
-    print(bin(ord(s)))
+conveyer1 = [5,4,3,2,1]
+conveyer2 = [10,9,8,7,6]
+conveyer3 = [15,14,13,12,11]
 
-bi = input()
-print(hex(int(bi,2)))
+def valid(cars):
+    for c in cars:
+        if conveyer1[-1] == c:
+            conveyer1.pop()
+        elif conveyer2[-1] == c:
+            conveyer2.pop()
+        elif conveyer3[-1] == c:
+            conveyer3.pop()
+        else:
+            print("infeasible")
